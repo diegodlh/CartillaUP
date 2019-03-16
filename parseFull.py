73a8d80b3e394b8bdca45e5e05f0d26d9b326c2b
Diego de la Hera <delahera@gmail.com>
2019-03-15 23:39:27 -0300

    Temporary log file tracks which records have been retrieved already so they are not retrieved again

--- parseFull.py
+++ parseFull.py
@@ -2,6 +2,7 @@ from lxml import html
 import requests
 import pdb
 import csv
+import os
 
 urlswitch = "https://www.unionpersonal.com.ar/modulos/afiliados/cartillas/switch.php"
 
@@ -54,35 +55,73 @@ def getPrestadores(params, data):
     return prestadores
 
 
-fieldnames = ['c', 'plan', 'cartilla', 'zona', 'categoria', 'especialidad', 'nombre', 'direccion', 'localidad', 'telefono']
-with open('out.csv', 'w') as fileout:
-    writer = csv.DictWriter(fileout, fieldnames=fieldnames)
-    writer.writeheader()
+def omitir(value, done):
+    if value in done:
+        print(' > OMITIR...')
+        return True
+    else:
+        print()
+        return False
+
+
+log_fieldnames = ['c', 'plan', 'cartilla', 'zona', 'categoria', 'especialidad']
+done = {fieldname: [] for fieldname in log_fieldnames}
+if os.path.exists('log.csv'):
+    with open('log.csv') as log_file:
+        log_reader = csv.DictReader(log_file)
+        for row in log_reader:
+            for fieldname, value in reversed(row.items()):
+                if value != '*':
+                    if value not in done[fieldname]:
+                        done[fieldname].append(value)
+                    break
+
+
+out_fieldnames = log_fieldnames + ['nombre', 'direccion', 'localidad', 'telefono']
+with open('out.csv', 'a') as out_file, open('log.csv', 'a') as log_file:
+    out_writer = csv.DictWriter(out_file, fieldnames=out_fieldnames)
+    if out_file.tell() == 0:
+        out_writer.writeheader()
+    log_writer = csv.DictWriter(log_file, fieldnames=log_fieldnames)
+    if log_file.tell() == 0:
+        log_writer.writeheader()
     params = {}
     data = {}
     for c in cs.keys():
-        print('.' + cs[c])
+        print('.' + cs[c], end='')
+        if omitir(c, done['c']):
+            continue
         params['c'] = c
         for plan in plans.keys():
-            print('..' + plans[plan])
+            print('..' + plans[plan], end='')
+            if omitir(plan, done['plan']):
+                continue
             data['plan'] = plan
             for cartilla in cartillas.keys():
-                print('...' + cartillas[cartilla])
+                print('...' + cartillas[cartilla], end='')
+                if omitir(cartilla, done['cartilla']):
+                    continue
                 params['control'] = 'zonas'
                 data['cartilla'] = cartilla
                 zonas = getOptions(params, data)
                 for zona in zonas.keys():
-                    print('....' + zonas[zona])
+                    print('....' + zonas[zona], end='')
+                    if omitir(zona, done['zona']):
+                        continue
                     params['control'] = 'categorias'
                     data['zona'] = zona
                     categorias = getOptions(params, data)
                     for categoria in categorias.keys():
-                        print('.....' + categorias[categoria])
+                        print('.....' + categorias[categoria], end='')
+                        if omitir(categoria, done['categoria']):
+                            continue
                         params['control'] = 'especialidades'
                         data['categoria'] = categoria
                         especialidades = getOptions(params, data)
                         for especialidad in especialidades.keys():
-                            print('......' + especialidades[especialidad])
+                            print('......' + especialidades[especialidad], end='')
+                            if omitir(especialidad, done['especialidad']):
+                                continue
                             params['control'] = 'consulta'
                             data['especialidad'] = especialidad
                             data['localidad'] = 'todas'
@@ -100,6 +139,19 @@ with open('out.csv', 'w') as fileout:
                                     'localidad': prestador['localidad'],
                                     'telefono': prestador['telefono']
                                 }
-                                writer.writerow(row)
-                            fileout.flush()
-                            # pdb.set_trace()
+                                out_writer.writerow(row)
+                            out_file.flush()
+                            del params['control']
+                            del data['localidad']
+                            log_writer.writerow({**params, **data})
+                        data['especialidad'] = '*'
+                        log_writer.writerow({**params, **data})
+                    data['categoria'] = '*'
+                    log_writer.writerow({**params, **data})
+                data['zona'] = '*'
+                log_writer.writerow({**params, **data})
+            data['cartilla'] = '*'
+            log_writer.writerow({**params, **data})
+        data['plan'] = '*'
+        log_writer.writerow({**params, **data})
+os.remove('log.csv')
