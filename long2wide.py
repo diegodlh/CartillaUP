0e7cb34309280be66d1f881d1c4165831c4e8457
Diego de la Hera <delahera@gmail.com>
2019-05-19 15:24:54 -0300

    empiezo a implementar menúes desplegables para plan y especialidad

--- long2wide.py
+++ long2wide.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import json
 import pdb
 
 df = pd.read_csv(
@@ -7,7 +8,8 @@ df = pd.read_csv(
   parse_dates=['accedido']
 )
 df.especialidad = df[['cartilla', 'categoria', 'especialidad']].apply(' > '.join, axis=1)
-
+especialidades = list(df.especialidad.unique())
+planes = list(df.plan.unique())
 
 def cat2code(df, fieldname):
   df[fieldname] = df[fieldname].astype('category')
@@ -52,22 +54,27 @@ df.accedido = df.groupby(['nombre', 'direccion', 'localidad', 'telefono', 'obser
 
 prestadores = df.groupby(['nombre', 'direccion', 'localidad', 'telefono', 'observaciones', 'coordenadas', 'plan', 'accedido']
   ).agg({'especialidad': set})
-prestadores = prestadores.unstack(level='plan')
+prestadores = prestadores.unstack(level='plan', fill_value=set())
 prestadores.columns = prestadores.columns.droplevel()
-# pdb.set_trace()
 prestadores = prestadores.reset_index()
+properties = [column for column in prestadores.columns if column not in planes + ['coordenadas']]
 
-properties = [column for column in prestadores.columns if column != 'coordenadas']
 geojson = prestadores[
   prestadores['coordenadas'].str.contains(r'^-?\d+.\d+,-?\d+.\d+$', regex=True)
 ].reset_index(drop=True)
+geojson['especialidades'] = geojson[planes].apply(lambda x: x.to_dict(), axis=1)
+properties += ['especialidades']
 geojson['properties'] = geojson[properties].apply(lambda x: x.to_dict(), axis=1)
 geojson['geometry'] = geojson['coordenadas'].apply(
   lambda x: {'type': 'Point', 'coordinates': [float(i) for i in x.split(',')][::-1]}  # geojson usa longitud, latitud
 )
 geojson['type'] = 'Feature'
-geojson = geojson.drop(properties + ['coordenadas'], axis=1)
+geojson = geojson.drop(planes + properties + ['coordenadas'], axis=1)
 
 prestadores.to_csv('prestadores.csv', index=None)
 prestadores.to_json('prestadores.json', orient='records')
 geojson.to_json('prestadores.geojson', orient='records')
+with open('especialidades.json', 'w') as fileout:
+  json.dump(especialidades, fileout)
+with open('planes.json', 'w') as fileout:
+  json.dump(planes, fileout)
